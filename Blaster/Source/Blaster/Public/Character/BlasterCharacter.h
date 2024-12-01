29ad8edcd4d00286772b41d7a42cc467f5a60cd6
Gihoon <89381041+Qussong@users.noreply.github.com>
2024-12-02 04:54:53 +0900

    Add Character Camera

--- Blaster/Source/Blaster/Public/Character/BlasterCharacter.h
+++ Blaster/Source/Blaster/Public/Character/BlasterCharacter.h
@@ -6,24 +6,29 @@
 #include "GameFramework/Character.h"
 #include "BlasterCharacter.generated.h"
 
+class USpringArmComponent;
+class UCameraComponent;
+
 UCLASS()
 class BLASTER_API ABlasterCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	ABlasterCharacter();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere, Category="Camera")
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, Category="Camera")
+	UCameraComponent* FollowCamera;
+
+public:
+	// Getter, Setter Section
 };
